4a216b3424352eaea63ace1bcef2b95f6c29833d
aazhuo <961006524@qq.com>
2018-09-07 14:22:07 +0800

    add usb_camera_core_file

---
+++ USB_Camera1/USB_Camera/Time.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+namespace USB_Camera
+{
+    class Time
+    {
+         public string  GetTime()
+            {
+            return DateTime.Now.Hour.ToString() +"-"+ DateTime.Now.Minute.ToString() +"-"+ DateTime.Now.Second.ToString();  
+             }
+
+
+    }
+}
